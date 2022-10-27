bfec69d98b4c8a58eb2bb223649c93ce447066dd
Andrés Gómez <andresgomez040106@gmail.com>
2022-10-27 12:45:07 -0500

    Creación de PrincipalController y sus vistas

---
+++ Facturacion_MVC/Controllers/PrincipalController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Facturacion_MVC.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        // GET: Principal
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Ayuda()
+        {
+            return View();
+        }
+        public ActionResult Acerca()
+        {
+            return View();
+        }
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+    }
+}
