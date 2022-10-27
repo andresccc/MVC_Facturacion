e61ea18b5da30461b890434a4c398d5d15f34e12
Andrés Gómez <andresgomez040106@gmail.com>
2022-10-27 12:58:54 -0500

    Se ajusta texto de cabecera en Layout

--- Facturacion_MVC/App_Start/RouteConfig.cs
+++ Facturacion_MVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Facturacion_MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Principal", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
